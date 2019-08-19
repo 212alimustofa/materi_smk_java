3f5e351bb65671edbe2df08503efef064fff7325
Ali Mustofa <mustofa@extrainteger.com>
2019-08-19 22:23:55 +0700

    init

---
+++ stringbuffer/StringBufferMethods.java
@@ -0,0 +1,7 @@
+public class StringBufferMethods {
+    public static void main(String[] args) {
+        String s1 = "A String";
+        StringBuffer s2 = new StringBuffer("A String from StringBuffer");
+        
+    }
+}
