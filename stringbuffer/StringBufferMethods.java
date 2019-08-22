d5c2fecb1ea3e2a6fd5a6a6dd5f6aede9f6739a1
Ali Mustofa <mustofa@extrainteger.com>
2019-08-22 10:00:31 +0700

    add new files

--- stringbuffer/StringBufferMethods.java
+++ stringbuffer/StringBufferMethods.java
@@ -1,7 +1,11 @@
 public class StringBufferMethods {
     public static void main(String[] args) {
-        String s1 = "A String";
-        StringBuffer s2 = new StringBuffer("A String from StringBuffer");
-        
+        StringBuffer s2 = new StringBuffer("hello ");
+        System.out.println(s2.append("world"));
+        System.out.println("\n");
+        System.out.println(s2.insert(1, "unwanted"));
+        System.out.println(s2.delete(1, 9));
+        System.out.println(s2.reverse);
+        System.out.println(s2.capacity());
     }
 }
