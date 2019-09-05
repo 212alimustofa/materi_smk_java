075a39a18f1af43354673f38eb4ee87f23500c97
Ali Mustofa <mustofa@extrainteger.com>
2019-09-05 11:10:03 +0700

    adding Swing Example

---
+++ swing/SwingExampleList.java
@@ -0,0 +1,21 @@
+import javax.swing.*;  
+public class SwingExampleList  { 
+     ListExample(){  
+        System.out.println("x");
+     }  
+public static void main(String args[])  {  
+   new ListExample();
+   JFrame frame= new JFrame();  
+        DefaultListModel<String> l1 = new DefaultListModel<>();  
+          l1.addElement("Item1");
+          l1.addElement("Item2");  
+          l1.addElement("Item3");  
+          l1.addElement("Item4");  
+          JList<String> list = new JList<>(l1);  
+          list.setBounds(0,0, 75,100);  
+          frame.add(list);  
+          frame.setSize(400,400);  
+          frame.setLayout(null);  
+          frame.setVisible(true); 
+}
+}  
