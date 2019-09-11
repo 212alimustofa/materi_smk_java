6b4078671233fad9ba6dd42d6a92db97db9c7b6f
Ali Mustofa <mustofa@extrainteger.com>
2019-09-11 23:53:19 +0700

    add new file

--- swing/SimpleGuiExample.java
+++ swing/SimpleGuiExample.java
@@ -1,16 +1,15 @@
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.*;
 
 public class SimpleGuiExample extends JFrame {
-
     private static final long serialVersionUID = 1L;
     private JLabel labelName, labelAge, labelAddress;
     private JTextField fieldName, fieldAge;
     private JTextArea fieldAddress;
     private JButton addButton;
+    private String combinedData = "";
 
     public static void main(String[] args) {
         new SimpleGuiExample();
@@ -22,12 +21,18 @@ public class SimpleGuiExample extends JFrame {
         setupAgeField();
         setupAddressField();
         setupButton();
+        showView();
+    }
+
+    private void showView() {
+        setVisible(true);
     }
 
     private void setupFrame() {
-        setSize(1366, 768);
-        setVisible(true);
+        setSize(340, 370);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Contoh Program Input Biodata");
+        getContentPane().setLayout(null);
     }
 
     private void setupNameField() {
@@ -63,20 +68,31 @@ public class SimpleGuiExample extends JFrame {
         // Add "Alamat" field to frame
         fieldAddress = new JTextArea();
         fieldAddress.setSize(200, 100); 
+        fieldAddress.setBounds(20, 160, 300, 120);
         getContentPane().add(fieldAddress);
     }
 
     private void setupButton() {
         // Add "Tambah" button to frame
         addButton = new JButton("Tambah");
-        addButton.setBounds(20, 420, 50, 30);
-        addButton.setSize(200, 100);
-        // addButton.addActionListener(new ActionListener(){
-        //     @Override
-        //     public void actionPerformed(ActionEvent e) {
-        //         JOptionPane().showMessageDialog(this, "Hello, Welcome to Javatpoint."); 
-        //     }
-        // });
-        getContentPane().add(addButton, BorderLayout.CENTER);
+        addButton.setBounds(20, 290, 50, 30);
+        addButton.setSize(100, 40);
+        addButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (fieldName.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(getContentPane(), "Nama tidak boleh kosong", "Hasil Input Kalian", JOptionPane.ERROR_MESSAGE);
+                } else if (fieldAge.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(getContentPane(), "Umur tidak boleh kosong", "Hasil Input Kalian", JOptionPane.ERROR_MESSAGE);
+                } else if (fieldAddress.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(getContentPane(), "Alamat tidak boleh kosong", "Hasil Input Kalian", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    combinedData = "Nama: "+fieldName.getText() 
+                    + "\n" +"Usia: "+ fieldAge.getText() + "\n" + "Alamat: "+fieldAddress.getText();
+                    JOptionPane.showMessageDialog(getContentPane(), combinedData, "Hasil Input Kalian", JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        });
+        getContentPane().add(addButton);
     }
 }
